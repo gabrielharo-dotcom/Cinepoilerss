acbc028bb9978555d6cbe07919718edfb0e6eb97
gabrielharo-dotcom <gabriel.haro@tecsup.edu.pe>
2026-04-18 19:25:34 -0500

    feat: creacion de modelos Movie y Spoiler con configuracion en admin

--- movies/admin.py
+++ movies/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Movie
 
-# Register your models here.
+
+@admin.register(Movie)
+class MovieAdmin(admin.ModelAdmin):
+    list_display = ('title', 'release_date', 'created_at')
+    search_fields = ('title',)
--- movies/models.py
+++ movies/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class Movie(models.Model):
+    title = models.CharField(max_length=200, verbose_name="Título")
+    description = models.TextField(verbose_name="Descripción")
+    release_date = models.DateField(verbose_name="Fecha de estreno")
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name = "Película"
+        verbose_name_plural = "Películas"
+        ordering = ('-release_date',)
+
+    def __str__(self):
+        return self.title
